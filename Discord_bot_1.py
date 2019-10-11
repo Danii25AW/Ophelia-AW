80f08a871ab83727737efa3964260da215bce369
Danii25AW <47294930+Danii25AW@users.noreply.github.com>
2019-10-11 12:59:41 +0300

    AW bot v 0.2

--- Discord_bot_1.py
+++ Discord_bot_1.py
@@ -1,13 +1,14 @@
 #
 # Example of simple discord chatbot based on discord.py library
 # For article (Russian language): http://tetraquark.ru/archives/377
+#
 
 import discord
 import asyncio
 import requests
 from discord.ext import commands
 
-TOKEN = 'NjMxOTMwNzAzNTU0MTUwNDE4.XZ-Bjg.RmT9v7iKBgfrEl5HAiF6uwsPvnE'
+TOKEN = 'NjMxOTMwNzAzNTU0MTUwNDE4.XaBQxA.rPX0hYFlRPujy0yQ2pTtY_msd0I'
 client = commands.Bot(command_prefix = '.')
 
 @client.event
@@ -16,10 +17,10 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-		
+
 @client.command(pass_context=True)
-async def clear(ctx, amount=5):
-	await ctx.channel.purge(limit=amount)
-	await ctx.send(f"Сообщения удалены {amount}")
+async def clear(ctx, amount=100):
+	await ctx.channel.purge(limit=amount + 1)
+	await ctx.send(f"Сообщений удалено: {amount}")
 
 client.run(TOKEN)
